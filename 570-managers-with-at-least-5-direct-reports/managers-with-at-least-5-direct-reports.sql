c4e7cd8aeca5f526044936a475fe0b16c9474ce1
Tejasvini Dhoble <124334652+tejasvinidhoble@users.noreply.github.com>
2025-02-28 22:09:51 +0530

    Time: 1610 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT e.name
+FROM Employee e
+JOIN (
+    SELECT managerId
+    FROM Employee
+    WHERE managerId IS NOT NULL
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+) m ON e.id = m.managerId;
